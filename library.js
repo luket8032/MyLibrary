ec299d88141c748b55743d47f91bd21ffa3cd15c
Luke <luket8032@hotmail.com>
2023-07-09 14:55:29 -0500

    Create form popup and close

--- library.js
+++ library.js
@@ -1,5 +1,8 @@
 let myLibrary = []
 const main = document.querySelector('.main')
+const popupForm = document.getElementById('popup')
+const addBtn = document.getElementById('addBtn')
+const closeBtn = document.getElementById('closeBtn')
 
 function Books(title, author, pages, read) {
     this.title = title
@@ -8,14 +11,11 @@ function Books(title, author, pages, read) {
     this.read = read
 }
 
-Books.prototype.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
-}
-
 function addCard(book) {
     const newCard = document.createElement('div')
     const cardInfo = document.createElement('div')
     const title = document.createElement('h3')
+    const divider = document.createElement('hr')
     const author = document.createElement('p')
     const pages = document.createElement('p')
     const status = document.createElement('p')
@@ -25,11 +25,20 @@ function addCard(book) {
     status.textContent = `Status: ${book.read}`
     newCard.classList.add('card');
     cardInfo.append(title);
+    cardInfo.append(divider)
     cardInfo.append(author);
     cardInfo.append(pages);
     cardInfo.append(status);
     newCard.append(cardInfo)
     main.append(newCard);
+}
+
+addBtn.onclick = function () {
+    popupForm.style.display = 'block'
+}
+
+closeBtn.onclick = function () {
+    popupForm.style.display = 'none'
 }
 
 const hobbit = new Books('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet')
