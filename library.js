e18562922bc9b1c3bec6c15e9589a73b5d0919f8
Luke <luket8032@hotmail.com>
2023-07-09 19:17:18 -0500

    Add remove button function

--- library.js
+++ library.js
@@ -5,6 +5,8 @@ const popupForm = document.getElementById('popup')
 const form = document.getElementById('book-form')
 const addBtn = document.getElementById('addBtn')
 const closeBtn = document.getElementById('closeBtn')
+const removeBtn = document.getElementById('removeBtn')
+const statusBtn = document.getElementById('statusBtn')
 
 function Books(title, author, pages, read) {
     this.title = title
@@ -21,16 +23,24 @@ function addCard(book) {
     const author = document.createElement('p')
     const pages = document.createElement('p')
     const status = document.createElement('p')
+    const statusBtn = document.createElement('button')
+    const removeBtn = document.createElement('button')
     title.textContent = book.title
     author.textContent = `Author: ${book.author}`
     pages.textContent = `Pages: ${book.pages}`
     status.textContent = `Status: ${book.read}`
+    statusBtn.textContent = 'Change Status'
+    removeBtn.textContent = 'Remove Book'
     newCard.classList.add('card')
+    statusBtn.setAttribute('id', 'statusBtn')
+    removeBtn.setAttribute('id', 'removeBtn')
     cardInfo.append(title)
     cardInfo.append(divider)
     cardInfo.append(author)
     cardInfo.append(pages)
     cardInfo.append(status)
+    cardInfo.append(statusBtn)
+    cardInfo.append(removeBtn)
     newCard.append(cardInfo)
     main.append(newCard)
 }
@@ -52,8 +62,6 @@ function addBook(e) {
     background.style.display = 'none'
 }
 
-popupForm.addEventListener('submit', addBook)
-
 addBtn.onclick = function () {
     popupForm.style.display = 'block'
     background.style.display = 'block'
@@ -64,11 +72,29 @@ closeBtn.onclick = function () {
     background.style.display = 'none'
 }
 
+popupForm.addEventListener('submit', addBook)
+
+document.addEventListener("click", function(e){
+    const targetRemove = e.target.closest("#removeBtn");
+    const targetChange = e.target.closest("#statusBtn"); // Or any other selector.
+  
+    if(targetRemove){
+      targetRemove.parentElement.parentElement.remove()
+    } else if (targetChange) {
+
+    }
+  });
+
+
 const hobbit = new Books('The Hobbit', 'J.R.R Tolkien', 295, 'read')
-const hobbit2 = new Books('The Hobbit 2', 'J.R.R Tolkien', 295, 'not read yet')
+const hobbit2 = new Books('The Hobbit 2', 'J.R.R Tolkien', 295, 'read')
+const test = new Books('test', 'test', 69, 'read')
+
 
 myLibrary.push(hobbit)
 myLibrary.push(hobbit2)
+
+
 
 for (let book in myLibrary) {
     const newCard = document.createElement('div')
@@ -78,16 +104,24 @@ for (let book in myLibrary) {
     const author = document.createElement('p')
     const pages = document.createElement('p')
     const status = document.createElement('p')
+    const statusBtn = document.createElement('button')
+    const removeBtn = document.createElement('button')
     title.textContent = myLibrary[book].title
     author.textContent = `Author: ${myLibrary[book].author}`
     pages.textContent = `Pages: ${myLibrary[book].pages}`
     status.textContent = `Status: ${myLibrary[book].read}`
+    statusBtn.textContent = 'Change Status'
+    removeBtn.textContent = 'Remove Book'
     newCard.classList.add('card')
+    statusBtn.setAttribute('id', 'statusBtn')
+    removeBtn.setAttribute('id', 'removeBtn')
     cardInfo.append(title)
     cardInfo.append(divider)
     cardInfo.append(author)
     cardInfo.append(pages)
     cardInfo.append(status)
+    cardInfo.append(statusBtn)
+    cardInfo.append(removeBtn)
     newCard.append(cardInfo)
     main.append(newCard)
 }
